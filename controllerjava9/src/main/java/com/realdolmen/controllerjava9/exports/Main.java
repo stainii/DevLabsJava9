7996cd79e72e744e3c7feb8a5d7f44d80bb07d82
stijnhooft@hotmail.com <stijnhoof@hotmail.com>
2017-10-20 17:16:15 +0200

    non-working code for controller

--- controllerjava9/src/main/java/com/realdolmen/controllerjava9/exports/Main.java
+++ controllerjava9/src/main/java/com/realdolmen/controllerjava9/exports/Main.java
@@ -1,4 +1,4 @@
-package com.realdolmen.secondjava9app;
+package com.realdolmen.controllerjava9.exports;
 
 import com.realdolmen.numberGenerator.exporting.NumberGenerator;
 
@@ -7,18 +7,18 @@ import java.util.List;
 public class Main {
 
     private final NumberGenerator numberGenerator;
-    private final RealdolmenFileReader fileReader;
+    //private final RealdolmenFileReader fileReader;
 
     public Main() {
-        fileReader = new RealdolmenFileReader();
+        //fileReader = new RealdolmenFileReader();
         numberGenerator = new NumberGenerator();
     }
 
     public String getRandomLine(String filePath) {
-        List<String> lines = fileReader.readFromFile(filePath);
+        List<String> lines = null;//fileReader.readFromFile(filePath);
 
         int numberOfLines = lines.size();
-        int randomNumber = numberGenerator.generateNumber(0, numberOfLines - 1;
+        int randomNumber = numberGenerator.generateNumber(0, numberOfLines - 1);
 
         return lines.get(randomNumber);
     }
